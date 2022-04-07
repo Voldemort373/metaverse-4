53d5211cf2ab40a9550db4e6feeab3fe815f5e8e
MJ LEE <mj1201276@yahoo.com.au>
2022-04-07 19:41:07 +1000

    Temporarily removed network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -31,24 +31,24 @@ module.exports = {
     tests: "./test",
   },
   networks:{
-    ganache: {
-      url: "http://127.0.0.1:8545",
-      chainId: 1337,
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
-    },
-    hardhat:{
-      chainId: 1337,
-      // gas: "auto",
-      // gasPrice: "auto",
-      // blockGasLimit: 100000000429720,
-    },
+    // ganache: {
+    //   url: "http://127.0.0.1:8545",
+    //   chainId: 1337,
+    //   accounts: [""]
+    // },
+    // hardhat:{
+    //   chainId: 1337,
+    //   // gas: "auto",
+    //   // gasPrice: "auto",
+    //   // blockGasLimit: 100000000429720,
+    // },
     // mumbai:{
     //   url: `https://polygon-mumbai.g.alchemy.com/v2/${ProjectId}`,
     //   accounts: privateKeyArray
     // }
-    rinkeby:{
-      url: `https://rinkeby.infura.io/v3/${ProjectId}`,
-      accounts: privateKeyArray,
-    }
+    // rinkeby:{
+    //   url: `https://rinkeby.infura.io/v3/${ProjectId}`,
+    //   accounts: privateKeyArray,
+    // }
   }
 };
